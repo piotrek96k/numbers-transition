db3d1b3b2f06f7e4a2f8743dbfa022fe9feab99f
Piotrek <piotrek960.pk@gmail.com>
2024-07-22 10:56:40 +0200

    Refactory story

--- src/NumbersTransition/NumbersTransition.stories.tsx
+++ src/NumbersTransition/NumbersTransition.stories.tsx
@@ -1,4 +1,5 @@
-import { Meta, StoryObj } from '@storybook/react';
+import { ArgTypes, Meta, StoryObj } from '@storybook/react';
+import { ComponentProps } from 'react';
 import NumbersTransition from './NumbersTransition';
 import { DecimalSeparator, DigitGroupSeparator } from './NumbersTransition.enum';
 
@@ -8,29 +9,33 @@ export default meta;
 
 type Story = StoryObj<typeof NumbersTransition>;
 
+const argTypes: Partial<ArgTypes<ComponentProps<typeof NumbersTransition>>> = {
+  digitGroupSeparator: {
+    options: Object.keys(DigitGroupSeparator),
+    mapping: DigitGroupSeparator,
+    control: {
+      type: 'select',
+    },
+  },
+  decimalSeparator: {
+    options: Object.keys(DecimalSeparator),
+    mapping: DecimalSeparator,
+    control: {
+      type: 'select',
+    },
+  },
+};
+
+const args: Partial<ComponentProps<typeof NumbersTransition>> = {
+  value: 123456789,
+  precision: 0,
+  horizontalAnimationDuration: 0.5,
+  verticalAnimationDuration: 2,
+  digitGroupSeparator: DigitGroupSeparator.SPACE,
+  decimalSeparator: DecimalSeparator.COMMA,
+};
+
 export const Primary: Story = {
-  args: {
-    value: 123456789,
-    precision: 0,
-    horizontalAnimationDuration: 0.5,
-    verticalAnimationDuration: 2,
-    digitGroupSeparator: DigitGroupSeparator.SPACE,
-    decimalSeparator: DecimalSeparator.COMMA,
-  },
-  argTypes: {
-    digitGroupSeparator: {
-      options: Object.keys(DigitGroupSeparator),
-      mapping: DigitGroupSeparator,
-      control: {
-        type: 'select',
-      },
-    },
-    decimalSeparator: {
-      options: Object.keys(DecimalSeparator),
-      mapping: DecimalSeparator,
-      control: {
-        type: 'select',
-      },
-    },
-  },
+  argTypes,
+  args,
 };
