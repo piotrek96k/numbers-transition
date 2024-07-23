0730f20190c891e0fed9aa69e7814bab511efe4b
Piotrek <piotrek960.pk@gmail.com>
2024-07-23 22:23:36 +0200

    Storybook refactor

--- src/NumbersTransition/NumbersTransition.stories.tsx
+++ src/NumbersTransition/NumbersTransition.stories.tsx
@@ -3,28 +3,36 @@ import { ComponentProps } from 'react';
 import NumbersTransition from './NumbersTransition';
 import { DecimalSeparator, DigitGroupSeparator } from './NumbersTransition.enum';
 
-const meta: Meta<typeof NumbersTransition> = { component: NumbersTransition };
-
-export default meta;
+type ComponentArgTypes = Partial<ArgTypes<ComponentProps<typeof NumbersTransition>>>;
 
 type Story = StoryObj<typeof NumbersTransition>;
 
-const argTypes: Partial<ArgTypes<ComponentProps<typeof NumbersTransition>>> = {
-  digitGroupSeparator: {
-    options: Object.keys(DigitGroupSeparator),
-    mapping: DigitGroupSeparator,
+const meta: Meta<typeof NumbersTransition> = { component: NumbersTransition };
+
+const inputTypeMapper = ([fieldName, enumObject]: [
+  keyof ComponentArgTypes,
+  typeof DigitGroupSeparator | typeof DecimalSeparator,
+]): ComponentArgTypes => ({
+  [fieldName]: {
+    options: Object.keys(enumObject),
+    mapping: enumObject,
     control: {
       type: 'select',
     },
   },
-  decimalSeparator: {
-    options: Object.keys(DecimalSeparator),
-    mapping: DecimalSeparator,
-    control: {
-      type: 'select',
-    },
-  },
-};
+});
+
+const argTypesReducer = (accumulator: ComponentArgTypes, currentValue: ComponentArgTypes): ComponentArgTypes => ({
+  ...accumulator,
+  ...currentValue,
+});
+
+const inputTypes: [keyof ComponentArgTypes, typeof DigitGroupSeparator | typeof DecimalSeparator][] = [
+  ['digitGroupSeparator', DigitGroupSeparator],
+  ['decimalSeparator', DecimalSeparator],
+];
+
+const argTypes: ComponentArgTypes = inputTypes.map<ComponentArgTypes>(inputTypeMapper).reduce(argTypesReducer);
 
 const args: Partial<ComponentProps<typeof NumbersTransition>> = {
   value: 123456789,
@@ -39,3 +47,5 @@ export const Primary: Story = {
   argTypes,
   args,
 };
+
+export default meta;
