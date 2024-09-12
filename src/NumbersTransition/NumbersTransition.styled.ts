9603663d51402549cbb59748d4ba51256cf009ca
Piotrek <piotrek960.pk@gmail.com>
2024-09-12 20:12:36 +0200

    Change imports order

--- src/NumbersTransition/NumbersTransition.styled.ts
+++ src/NumbersTransition/NumbersTransition.styled.ts
@@ -1,11 +1,11 @@
-import styled, { RuleSet, css, keyframes } from 'styled-components';
-import { IStyledComponentBase, FastOmit, Substitute, Keyframes } from 'styled-components/dist/types';
 import {
   RefObject,
   HTMLAttributes,
   DetailedHTMLProps,
   DO_NOT_USE_OR_YOU_WILL_BE_FIRED_CALLBACK_REF_RETURN_VALUES,
 } from 'react';
+import styled, { RuleSet, css, keyframes } from 'styled-components';
+import { IStyledComponentBase, FastOmit, Substitute, Keyframes } from 'styled-components/dist/types';
 import {
   StyledComponentType,
   AnimationType,
