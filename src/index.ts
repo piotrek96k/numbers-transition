4a5a4b47f9ede2356669f2059dbece3d8642d8b1
Piotrek <piotrek960.pk@gmail.com>
2024-11-26 11:51:49 +0100

    import refactor

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import NumbersTransition from './NumbersTransition/NumbersTransition';
 import {
   DecimalSeparator,
   DigitGroupSeparator,
   NegativeCharacter,
   NegativeCharacterAnimationMode,
 } from './NumbersTransition/NumbersTransition.enums';
+import NumbersTransition from './NumbersTransition/NumbersTransition';
 import { BigDecimal } from './NumbersTransition/NumbersTransition.types';
 
 export { DecimalSeparator, DigitGroupSeparator, NegativeCharacter, NegativeCharacterAnimationMode, NumbersTransition };
