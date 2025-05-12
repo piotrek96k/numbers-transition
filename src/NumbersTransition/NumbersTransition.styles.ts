e5ad579dbda50cfa4da3ceb9bc20a2cfdc7e0e1b
Piotrek <piotrek960.pk@gmail.com>
2025-05-12 23:52:08 +0200

    Change type in theme

--- src/NumbersTransition/NumbersTransition.styles.ts
+++ src/NumbersTransition/NumbersTransition.styles.ts
@@ -14,6 +14,7 @@ import {
   AnimationType,
   Display,
   HorizontalAnimationDirection,
+  NumberOfAnimations,
   Numbers,
   Runtime,
   Strings,
@@ -46,7 +47,7 @@ export type AttributesStyledComponent<
 
 export interface NumbersTransitionTheme {
   $animationType?: AnimationType;
-  $numberOfAnimations?: number;
+  $numberOfAnimations?: NumberOfAnimations;
   $totalAnimationDuration?: number;
   $horizontalAnimationDuration?: number;
   $verticalAnimationDuration?: number;
