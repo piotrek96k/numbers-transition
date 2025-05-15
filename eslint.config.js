9f753c36738499dbaf4d75c855f7d454275745fc
Piotrek <piotrek960.pk@gmail.com>
2025-05-15 23:52:10 +0200

    Eslint refactor

--- eslint.config.js
+++ eslint.config.js
@@ -8,10 +8,8 @@ import tsParser from '@typescript-eslint/parser';
 import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: path.dirname(fileURLToPath(import.meta.url)),
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all,
 });
