438616e914da50902ef3f2fcd5e83fd0858bf479
Piotrek <piotrek960.pk@gmail.com>
2025-06-16 19:44:40 +0200

    Update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import path from 'node:path';
+import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
 import _import from 'eslint-plugin-import';
@@ -9,7 +9,7 @@ import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
 
 const compat = new FlatCompat({
-  baseDirectory: path.dirname(fileURLToPath(import.meta.url)),
+  baseDirectory: dirname(fileURLToPath(import.meta.url)),
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all,
 });
@@ -20,32 +20,32 @@ export default [
     compat.extends(
       'eslint:recommended',
       'plugin:prettier/recommended',
-      'plugin:@typescript-eslint/recommended',
       'plugin:import/typescript',
       'plugin:react-hooks/recommended',
       'plugin:storybook/recommended',
+      'plugin:@typescript-eslint/recommended',
     ),
   ),
   {
     plugins: { import: fixupPluginRules(_import), 'react-refresh': reactRefresh },
     languageOptions: { globals: { ...globals.browser }, parser: tsParser },
     rules: {
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       quotes: ['error', 'single', { avoidEscape: true }],
       'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-      'no-duplicate-imports': 'error',
       'import/order': 'error',
       'import/no-duplicates': 'error',
       'sort-imports': ['error', { ignoreDeclarationSort: true }],
-      'object-shorthand': 'error',
+      'no-duplicate-imports': 'error',
       'no-use-before-define': 'error',
       'no-useless-rename': 'error',
       'no-useless-concat': 'error',
       'prefer-template': 'error',
+      'object-shorthand': 'error',
       'prefer-spread': 'error',
       '@typescript-eslint/no-unused-vars': 'error',
       '@typescript-eslint/no-empty-object-type': ['error', { allowInterfaces: 'with-single-extends' }],
       'no-console': 'warn',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@typescript-eslint/no-explicit-any': 'off',
     },
   },
