33bd91454f13d8df7a71898e4dfa08111bbae3f5
Piotrek <piotrek960.pk@gmail.com>
2025-10-10 02:14:24 +0200

    Fix default import for cjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import { resolve } from 'path';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import react from '@vitejs/plugin-react-swc';
@@ -6,7 +7,10 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react(), dts({ rollupTypes: true })],
   build: {
-    lib: { entry: resolve(__dirname, 'src/index.ts'), name: 'numbers-transition' },
-    rollupOptions: { external: ['react', 'react-dom', 'styled-components'], output: [{ format: 'es' }, { format: 'cjs' }] },
+    lib: { entry: resolve(dirname(fileURLToPath(import.meta.url)), 'src/index.ts'), name: 'numbers-transition' },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'styled-components'],
+      output: [{ format: 'es' }, { format: 'cjs', exports: 'named' }],
+    },
   },
 });
