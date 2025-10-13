82254677f0d2cfca1d458d240be9ddba0b35b566
Piotrek <piotrek960.pk@gmail.com>
2025-10-13 23:22:52 +0200

    Add rotation example to storybook

--- src/NumbersTransition/NumbersTransition.stories.tsx
+++ src/NumbersTransition/NumbersTransition.stories.tsx
@@ -3,9 +3,12 @@ import { RuleSet, css } from 'styled-components';
 import { ArgTypes, Meta, StoryObj } from '@storybook/react-vite';
 import NumbersTransition from './NumbersTransition';
 import {
+  AnimationDirection,
   AnimationId,
   AnimationInterruptionMode,
+  AnimationNumber,
   AnimationTimingFunction,
+  AnimationType,
   Character,
   DecimalSeparatorCharacter,
   DigitGroupSeparatorCharacter,
@@ -17,7 +20,14 @@ import {
   VariableName,
 } from './NumbersTransition.enums';
 import { AnimationDuration, ExtendedAnimationTimingFunction } from './NumbersTransition.hooks';
-import { Animation, AnimationFactory, EasingFunction, NumbersTransitionExecutionContext } from './NumbersTransition.styles';
+import {
+  Animation,
+  AnimationFactory,
+  CssRuleFactory,
+  EasingFunction,
+  NumbersTransitionExecutionContext,
+  NumbersTransitionTheme,
+} from './NumbersTransition.styles';
 import { Falsy, OrReadOnly } from './NumbersTransition.types';
 
 type NumbersTransitionProps = typeof NumbersTransition<
@@ -28,7 +38,7 @@ type NumbersTransitionProps = typeof NumbersTransition<
   object,
   unknown,
   object,
-  unknown,
+  number,
   object,
   unknown,
   object,
@@ -140,10 +150,10 @@ const opacityAnimationFactory: AnimationFactory<object, number> = ({
 
 const opacityAnimationCss: RuleSet<object> = css<object>`
   ${basicEffectCss};
-  animation-duration: calc(var(${VariableName.TotalAnimationDuration}) / ${Integer.Two});
   animation-direction: alternate;
-  animation-iteration-count: ${Integer.Two};
   animation-fill-mode: forwards;
+  animation-duration: calc(var(${VariableName.TotalAnimationDuration}) / ${Integer.Two});
+  animation-iteration-count: ${Integer.Two};
 `;
 
 const opacityAnimationArgs: ComponentProps<NumbersTransitionProps> = {
@@ -152,5 +162,88 @@ const opacityAnimationArgs: ComponentProps<NumbersTransitionProps> = {
 };
 
 export const OpacityAnimation: Story = { argTypes, args: opacityAnimationArgs };
+
+const rotateKeyframeFunction = (keyframeValue: number): RuleSet<object> => css<object>`
+  transform: rotateY(${keyframeValue}turn);
+`;
+
+const getHorizontalRotateKeyframes = ({ animationNumber, animationDuration, totalAnimationDuration }: NumbersTransitionTheme): number[] =>
+  animationNumber === AnimationNumber.One
+    ? [Integer.Zero, animationDuration! / totalAnimationDuration! / Integer.Two]
+    : [(Integer.One + (totalAnimationDuration! - animationDuration!) / totalAnimationDuration!) / Integer.Two, Integer.One];
+
+const horizontalRotateAnimationFactory: AnimationFactory<object, number> = ({
+  theme: { animationType, ...restTheme },
+}: NumbersTransitionExecutionContext): Animation<object, number> | Falsy =>
+  animationType === AnimationType.Horizontal && {
+    keyframeFunction: rotateKeyframeFunction,
+    keyframes: getHorizontalRotateKeyframes(restTheme),
+  };
+
+const getVerticalRotateKeyframes = ({
+  animationNumber,
+  horizontalAnimationDuration,
+  verticalAnimationDuration,
+  totalAnimationDuration,
+}: NumbersTransitionTheme): number[] => [
+  animationNumber === AnimationNumber.One ? Integer.Zero : horizontalAnimationDuration! / totalAnimationDuration!,
+  animationNumber === AnimationNumber.One
+    ? verticalAnimationDuration! / totalAnimationDuration!
+    : (horizontalAnimationDuration! + verticalAnimationDuration!) / totalAnimationDuration!,
+];
+
+const mapVerticalRotateKeyframes =
+  ({ animationDirection, columnLength, rowIndex }: NumbersTransitionTheme): ((value: number) => number) =>
+  (value: number): number =>
+    value +
+    (columnLength! / Integer.Two === rowIndex! + Integer.One / Integer.Two
+      ? Integer.One / Integer.Two
+      : ((animationDirection === AnimationDirection.Normal ? Integer.Zero : Integer.One) + Math.min(rowIndex!, columnLength! - rowIndex!)) %
+        Integer.Two);
+
+const verticalRotateAnimationFactory: AnimationFactory<object, number> = ({
+  theme: { animationType, columnLength, ...restTheme },
+}: NumbersTransitionExecutionContext): Animation<object, number> | Falsy =>
+  animationType === AnimationType.Vertical &&
+  columnLength! > Integer.One && {
+    keyframeFunction: rotateKeyframeFunction,
+    keyframes: getVerticalRotateKeyframes(restTheme)
+      .map<number>(mapVerticalRotateKeyframes({ ...restTheme, columnLength }))
+      .map<number>((value: number): number => value / Integer.Two),
+  };
+
+const getInitialRotation = ({
+  animationNumber,
+  animationType,
+  horizontalAnimationDuration,
+  verticalAnimationDuration,
+  totalAnimationDuration,
+}: NumbersTransitionTheme): number => {
+  switch (animationNumber!) {
+    case AnimationNumber.Three:
+      return (horizontalAnimationDuration! + verticalAnimationDuration!) / totalAnimationDuration!;
+    case AnimationNumber.Two:
+      return animationType === AnimationType.Horizontal
+        ? verticalAnimationDuration! / totalAnimationDuration!
+        : horizontalAnimationDuration! / totalAnimationDuration!;
+    default:
+      return Integer.Zero;
+  }
+};
+
+const rotateAnimationCssFactory: CssRuleFactory<object> = ({ theme }: NumbersTransitionExecutionContext): RuleSet<object> => css<object>`
+  ${rotateKeyframeFunction(getInitialRotation(theme) / Integer.Two)};
+  animation-direction: normal;
+  animation-fill-mode: forwards;
+  animation-duration: var(${VariableName.AnimationDuration});
+  animation-iteration-count: ${Integer.One};
+`;
+
+const rotateAnimationArgs: ComponentProps<NumbersTransitionProps> = {
+  ...basicEffectArgs,
+  digitView: { css: rotateAnimationCssFactory, animation: [horizontalRotateAnimationFactory, verticalRotateAnimationFactory] },
+};
+
+export const RotateAnimation: Story = { argTypes, args: rotateAnimationArgs };
 
 export default meta;
