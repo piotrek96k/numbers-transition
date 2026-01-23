f69033ccefd6bdef18e561f6ccc3e48cb46fb9e3
Piotrek <piotrek960.pk@gmail.com>
2026-01-23 16:08:08 +0100

    styled refactor

--- src/styled.ts
+++ src/styled.ts
@@ -11,11 +11,11 @@ import type {
 import type { CSSProperties, FastOmit, IStyledComponent, SupportedHTMLElements } from 'styled-components';
 import type { Runtime } from './NumbersTransition/NumbersTransition.enums';
 import type { NumbersTransitionTheme } from './NumbersTransition/NumbersTransition.styles';
-import type { Nullable } from './NumbersTransition/NumbersTransition.types';
+import type { Nullable, Optional } from './NumbersTransition/NumbersTransition.types';
 
 declare module 'styled-components' {
   export type BaseObject = {};
-  export type CSSPropertiesWithVars = CSSProperties & { [key: `--${string}`]: string | number | undefined };
+  export type CSSPropertiesWithVars = CSSProperties & { [key: `--${string}`]: Optional<string | number> };
   export type StyledHTMLAttributes<T> = HTMLAttributes<T> & { style: CSSPropertiesWithVars };
   export interface DefaultTheme extends NumbersTransitionTheme {}
 
@@ -25,14 +25,14 @@ declare module 'styled-components' {
     rules: string;
   }
 
-  interface ExoticComponentWithDisplayName<P extends BaseObject = {}> extends ExoticComponent<P> {
-    defaultProps?: Partial<P> | undefined;
-    displayName?: string | undefined;
+  interface ExoticComponentWithDisplayName<T extends BaseObject = {}> extends ExoticComponent<T> {
+    defaultProps?: Partial<T>;
+    displayName?: string;
   }
 
-  type AnyComponent<P extends BaseObject = any> = ExoticComponentWithDisplayName<P> | ComponentType<P>;
+  type AnyComponent<T extends BaseObject = any> = ExoticComponentWithDisplayName<T> | ComponentType<T>;
   type KnownTarget = SupportedHTMLElements | AnyComponent;
-  type Substitute<A extends BaseObject, B extends BaseObject> = FastOmit<A, keyof B> & B;
+  type Substitute<T extends BaseObject, U extends BaseObject> = FastOmit<T, keyof U> & U;
   type StyledComponentBase<T extends object> = IStyledComponent<Runtime.Web, T>;
   type HTMLDetailedElement<T> = DetailedHTMLProps<HTMLAttributes<T>, T>;
 
