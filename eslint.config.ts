f17ceef559d6e25a4e1c77202846b1e6b5fd6b73
Piotrek <piotrek960.pk@gmail.com>
2026-05-01 00:10:07 +0200

    More refactor

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,4 @@
+import type { RulesConfig } from '@eslint/core';
 import js from '@eslint/js';
 import perfectionist from 'eslint-plugin-perfectionist';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
@@ -6,6 +7,23 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+
+const rules: RulesConfig = {
+  '@typescript-eslint/no-empty-object-type': ['error', { allowInterfaces: 'with-single-extends', allowWithName: 'BaseObject' }],
+  '@typescript-eslint/no-explicit-any': 'off',
+  'no-console': 'warn',
+  'no-duplicate-imports': 'error',
+  'no-use-before-define': ['error', { classes: false }],
+  'no-useless-concat': 'error',
+  'no-useless-rename': 'error',
+  'object-shorthand': 'error',
+  'perfectionist/sort-imports': ['error', { type: 'alphabetical', order: 'asc', newlinesBetween: 'ignore' }],
+  'prefer-spread': 'error',
+  'prefer-template': 'error',
+  'react-hooks/refs': 'off',
+  'react-hooks/set-state-in-effect': 'off',
+  'sort-imports': ['error', { ignoreDeclarationSort: true }],
+};
 
 const config: Parameters<typeof defineConfig> = [
   globalIgnores(['dist', 'plugins/dist', 'storybook-static']),
@@ -21,22 +39,7 @@ const config: Parameters<typeof defineConfig> = [
     languageOptions: { globals: globals.browser },
     linterOptions: { reportUnusedDisableDirectives: 'error' },
     plugins: { perfectionist },
-    rules: {
-      '@typescript-eslint/no-empty-object-type': ['error', { allowInterfaces: 'with-single-extends', allowWithName: 'BaseObject' }],
-      '@typescript-eslint/no-explicit-any': 'off',
-      'no-console': 'warn',
-      'no-duplicate-imports': 'error',
-      'no-use-before-define': ['error', { classes: false }],
-      'no-useless-concat': 'error',
-      'no-useless-rename': 'error',
-      'object-shorthand': 'error',
-      'perfectionist/sort-imports': ['error', { type: 'alphabetical', order: 'asc', newlinesBetween: 'ignore' }],
-      'prefer-spread': 'error',
-      'prefer-template': 'error',
-      'react-hooks/refs': 'off',
-      'react-hooks/set-state-in-effect': 'off',
-      'sort-imports': ['error', { ignoreDeclarationSort: true }],
-    },
+    rules,
   },
 ];
 
