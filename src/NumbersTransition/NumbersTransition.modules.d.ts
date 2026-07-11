a782bb8e3c2a3276d49b1733757978bc008d57cb
Piotr Karwowski <piotr.karwowski@sollers.eu>
2026-07-11 02:26:40 +0200

    Add first and last to Array

--- src/NumbersTransition/NumbersTransition.modules.d.ts
+++ src/NumbersTransition/NumbersTransition.modules.d.ts
@@ -1,6 +1,18 @@
 import type { ReactElement, ReactNode } from 'react';
 import type { NumbersTransitionTheme } from './NumbersTransition.styles';
-import type { ArrayOfDepth, Assert, At, Last, Nullable, Optional, OrArray, OrFunction, OrReadOnly, Zip } from './NumbersTransition.types';
+import type {
+  ArrayOfDepth,
+  Assert,
+  At,
+  First,
+  Last,
+  Nullable,
+  Optional,
+  OrArray,
+  OrFunction,
+  OrReadOnly,
+  Zip,
+} from './NumbersTransition.types';
 
 declare global {
   interface Boolean {
@@ -62,6 +74,7 @@ declare global {
     filterEach(...predicates: ((value: T, index: number, array: T[]) => boolean)[]): T[];
     findMap<U>(mapper: (value: T, index: number, array: T[]) => Optional<U>): Optional<U>;
     findMap<U>(mapper: (value: T, index: number, array: T[]) => Optional<U>, fallback: U): U;
+    first(): First<this>;
     flatMap<U, V extends U[], W = undefined>(
       callback: (this: W, value: T, index: number, array: T[]) => U | ReadonlyArray<U>,
       thisArg?: W,
@@ -69,6 +82,7 @@ declare global {
     forEach(callbackfn: (value: T, index: number, array: T[]) => unknown, thisArg?: unknown): void;
     insert(value: T, index: number): T[];
     intersects(array: T[]): boolean;
+    last(): Last<this>;
     map<U>(callbackfn: (value: T, index: number, array: T[]) => U, thisArg?: unknown): { [I in keyof this]: U };
     map<U, V extends U[]>(callbackfn: (value: T, index: number, array: T[]) => U, thisArg?: unknown): V;
     mapEach(...mappers: ((val: T, idx: number, array: T[]) => T)[]): T[];
