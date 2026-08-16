b43a2b30aa8bda3f38a1bbaaf7ddb73f1d054ff5
Piotr Karwowski <piotrek960.pk@gmail.com>
2026-08-16 05:01:43 +0200

    Fix formatting

--- src/NumbersTransition/NumbersTransition.components.tsx
+++ src/NumbersTransition/NumbersTransition.components.tsx
@@ -423,9 +423,9 @@ export const NumberElement = <Q extends object, R, S extends object, T, U extend
   const mapToDigitThemeProviderElement: ElementKeyMapper<OrArray<ReactElement>> = useElementKeyMapper<OrArray<ReactElement>, ThemeProviderProps>(
     ThemeProvider,
     (_: OrArray<ReactElement>, digitIndex: number, { length }: OrArray<ReactElement>[]): ThemeProviderProps => ({
-    theme: { characterIndex: getCharacterIndex(digitIndex, length), digitIndex },
-  }),
-);
+      theme: { characterIndex: getCharacterIndex(digitIndex, length), digitIndex },
+    }),
+  );
 
   const mapToDigitsThemeProviderElement: ElementKeyMapper<ReactElement> = useElementKeyMapper<ReactElement, ThemeProviderProps>(
     ThemeProvider,
