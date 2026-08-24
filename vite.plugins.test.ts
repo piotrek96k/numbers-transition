57a2f71411ae8a83b34cbf00bfc6ef42b27f5cf4
Piotr Karwowski <piotrek960.pk@gmail.com>
2026-08-25 00:32:01 +0200

    Move tests separately

--- vite.plugins.test.ts
+++ vite.plugins.test.ts
@@ -6,13 +6,13 @@ import typeExtensions from './plugins/dist/type-extensions.js';
 const mapAlias = ([key, value]: [string, string]): [string, string] => [`type-extensions/${key}`, resolve('plugins', 'dist', value)];
 
 const config: UserConfig = {
-  plugins: [typeExtensions('tsconfig.plugins.test.json', resolve('plugins', 'tests', 'type-extensions', 'extensions', 'extensions.ts'))],
+  plugins: [typeExtensions('tsconfig.plugins.test.json', resolve('tests', 'type-extensions', 'extensions', 'extensions.ts'))],
   resolve: {
     alias: Object.fromEntries<string>(
       Object.entries<string>({ plugin: 'type-extensions', extension: 'extension' }).map<[string, string]>(mapAlias),
     ),
   },
-  test: { globals: true, environment: 'node', include: ['plugins/tests/**/*.test.ts'] },
+  test: { globals: true, environment: 'node', include: ['tests/**/*.test.ts'] },
 };
 
 export default defineConfig(config);
